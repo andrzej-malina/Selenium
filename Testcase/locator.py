b504d784423279b9f04b17a685b459ed829d0a9d
Andrzej Jankowski <andrzej.malina@gmail.com>
2021-02-05 15:42:01 +0100

    test for smaller buttons added, refactored

--- Testcase/locator.py
+++ Testcase/locator.py
@@ -1,11 +1,26 @@
-#Klasa do definiowania lokalizacji na testowanej stronie
+# Klasa do definiowania lokalizacji na testowanej stronie
 
 from selenium.webdriver.common.by import By
 
+
 class MainPageLocators(object):
-    SEARCH_BUTTON = (By.CSS_SELECTOR, "input[placeholder='What you search for...']")
+    SEARCH_FIELD = (By.CSS_SELECTOR, "input[placeholder='What you search for...']")
+    SEARCH_BUTTON = (By.XPATH, "/html/body/app-root/app-index/main/app-index/form/div[2]/button")
     NETWORK_BUTTON = (By.PARTIAL_LINK_TEXT, "network")
     PROFILE_BUTTON = (By.PARTIAL_LINK_TEXT, "profile")
     MOVIES_BUTTON = (By.PARTIAL_LINK_TEXT, "movies")
     PUBLICATIONS_BUTTON = (By.PARTIAL_LINK_TEXT, "publications")
     ACTIVITIES_BUTTON = (By.XPATH, "/html/body/app-root/app-index/main/app-index/form[2]/label[1]")
+    ANATOMY_BUTTON = (By.XPATH, "/html/body/app-root/app-index/main/app-index/form[2]/label[2]")
+    CHEMICALS_BUTTON = (By.XPATH, "/html/body/app-root/app-index/main/app-index/form[2]/label[3]")
+    CONCEPTS_BUTTON = (By.XPATH, "/html/body/app-root/app-index/main/app-index/form[2]/label[4]")
+    DEVICES_BUTTON = (By.XPATH, "/html/body/app-root/app-index/main/app-index/form[2]/label[5]")
+    DISORDERS_BUTTON = (By.XPATH, "/html/body/app-root/app-index/main/app-index/form[2]/label[6]")
+    GEO_BUTTON = (By.XPATH, "/html/body/app-root/app-index/main/app-index/form[2]/label[7]")
+    LIVING_BUTTON = (By.XPATH, "/html/body/app-root/app-index/main/app-index/form[2]/label[8]")
+    OBJECTS_BUTTON = (By.XPATH, "/html/body/app-root/app-index/main/app-index/form[2]/label[9]")
+    ORGANIZATIONS_BUTTON = (By.XPATH, "/html/body/app-root/app-index/main/app-index/form[2]/label[10]")
+    PHENOMENA_BUTTON = (By.XPATH, "/html/body/app-root/app-index/main/app-index/form[2]/label[11]")
+    PHYSIOLOGY_BUTTON = (By.XPATH, "/html/body/app-root/app-index/main/app-index/form[2]/label[12]")
+    PROCEDURES_BUTTON = (By.XPATH, "/html/body/app-root/app-index/main/app-index/form[2]/label[13]")
+    SHOWALL_BUTTON = (By.XPATH, "/html/body/app-root/app-index/main/app-index/form[2]/button")
--- Testcase/main2.py
+++ Testcase/main2.py
@@ -2,9 +2,10 @@
 
 import unittest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.by import By
 import page
 import time
+from locator import *
 
 
 class PythonOrgSearch(unittest.TestCase):
@@ -13,10 +14,10 @@ class PythonOrgSearch(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome('C:/Users/ajankowski/Downloads/chromedriver')
         self.driver.get('http://brainiverse.cktech.eu/search')
-        search_field = self.driver.find_element(By.CSS_SELECTOR, "input[placeholder='What you search for...']")
+        search_field = self.driver.find_element(*(MainPageLocators.SEARCH_FIELD))
         search_field.clear()
         search_field.send_keys('covid')
-        search_button = self.driver.find_element(By.XPATH, "/html/body/app-root/app-index/main/app-index/form/div[2]/button")
+        search_button = self.driver.find_element(*(MainPageLocators.SEARCH_BUTTON))
         search_button.click()
         time.sleep(2)
 
@@ -33,10 +34,10 @@ class PythonOrgSearch(unittest.TestCase):
 
     # funkcja sprawdzająca czy są wyniki wyszukiwania dla bezsensownego zapytania 'zapytanie o jakies glupoty'
     def test3_search_stupid(self):
-        search_field = self.driver.find_element(By.CSS_SELECTOR, "input[placeholder='What you search for...']")
+        search_field = self.driver.find_element(*(MainPageLocators.SEARCH_FIELD))
         search_field.clear()
         search_field.send_keys('zapytanie o jakies glupoty')
-        search_button = self.driver.find_element(By.XPATH, "/html/body/app-root/app-index/main/app-index/form/div[2]/button")
+        search_button = self.driver.find_element(*(MainPageLocators.SEARCH_BUTTON))
         search_button.click()
         time.sleep(3)
         assert "Covid" not in self.driver.page_source
@@ -71,103 +72,102 @@ class PythonOrgSearch(unittest.TestCase):
 
     # funkcja do sprawdzenia przycisku activities
     def test8_activities_button(self):
-        activities_button = self.driver.find_element(By.XPATH, "/html/body/app-root/app-index/main/app-index/form[2]/label[1]")
+        activities_button = self.driver.find_element(*(MainPageLocators.ACTIVITIES_BUTTON))
         activities_button.click()
         time.sleep(2)
 
         assert activities_button.get_attribute("class") == 'ng-star-inserted'
 
     def test9_anatomy_button(self):
-        anatomy_button = self.driver.find_element(By.XPATH, "/html/body/app-root/app-index/main/app-index/form[2]/label[2]")
+        anatomy_button = self.driver.find_element(*(MainPageLocators.ANATOMY_BUTTON))
         anatomy_button.click()
         time.sleep(2)
 
         assert anatomy_button.get_attribute("class") == 'ng-star-inserted'
 
     def test10_chemicals_button(self):
-        chemicals_button = self.driver.find_element(By.XPATH, "/html/body/app-root/app-index/main/app-index/form[2]/label[3]")
+        chemicals_button = self.driver.find_element(*(MainPageLocators.CHEMICALS_BUTTON))
         chemicals_button.click()
         time.sleep(2)
 
         assert chemicals_button.get_attribute("class") == 'ng-star-inserted'
 
     def test11_concepts_button(self):
-        concepts_button = self.driver.find_element(By.XPATH, "/html/body/app-root/app-index/main/app-index/form[2]/label[4]")
+        concepts_button = self.driver.find_element(*(MainPageLocators.CONCEPTS_BUTTON))
         concepts_button.click()
         time.sleep(2)
 
         assert concepts_button.get_attribute("class") == 'ng-star-inserted'
 
     def test12_devices_button(self):
-        devices_button = self.driver.find_element(By.XPATH, "/html/body/app-root/app-index/main/app-index/form[2]/label[5]")
+        devices_button = self.driver.find_element(*(MainPageLocators.DEVICES_BUTTON))
         devices_button.click()
         time.sleep(2)
 
         assert devices_button.get_attribute("class") == 'ng-star-inserted'
 
     def test13_disorders_button(self):
-        disorders_button = self.driver.find_element(By.XPATH, "/html/body/app-root/app-index/main/app-index/form[2]/label[6]")
+        disorders_button = self.driver.find_element(*(MainPageLocators.DISORDERS_BUTTON))
         disorders_button.click()
         time.sleep(2)
 
         assert disorders_button.get_attribute("class") == 'ng-star-inserted'
 
     def test14_geo_button(self):
-        geo_button = self.driver.find_element(By.XPATH, "/html/body/app-root/app-index/main/app-index/form[2]/label[7]")
+        geo_button = self.driver.find_element(*(MainPageLocators.GEO_BUTTON))
         geo_button.click()
         time.sleep(2)
 
         assert geo_button.get_attribute("class") == 'ng-star-inserted'
 
-
     def test15_living_button(self):
-        living_button = self.driver.find_element(By.XPATH, "/html/body/app-root/app-index/main/app-index/form[2]/label[8]")
+        living_button = self.driver.find_element(*(MainPageLocators.LIVING_BUTTON))
         living_button.click()
         time.sleep(2)
 
         assert living_button.get_attribute("class") == 'ng-star-inserted'
 
     def test16_objects_button(self):
-        objects_button = self.driver.find_element(By.XPATH, "/html/body/app-root/app-index/main/app-index/form[2]/label[9]")
+        objects_button = self.driver.find_element(*(MainPageLocators.OBJECTS_BUTTON))
         objects_button.click()
         time.sleep(2)
 
         assert objects_button.get_attribute("class") == 'ng-star-inserted'
 
     def test17_organizations_button(self):
-        organizations_button = self.driver.find_element(By.XPATH, "/html/body/app-root/app-index/main/app-index/form[2]/label[10]")
+        organizations_button = self.driver.find_element(*(MainPageLocators.ORGANIZATIONS_BUTTON))
         organizations_button.click()
         time.sleep(2)
 
         assert organizations_button.get_attribute("class") == 'ng-star-inserted'
 
     def test18_phenomena_button(self):
-        phenomena_button = self.driver.find_element(By.XPATH, "/html/body/app-root/app-index/main/app-index/form[2]/label[11]")
+        phenomena_button = self.driver.find_element(*(MainPageLocators.PHENOMENA_BUTTON))
         phenomena_button.click()
         time.sleep(2)
 
         assert phenomena_button.get_attribute("class") == 'ng-star-inserted'
 
     def test19_psychology_button(self):
-        psychology_button = self.driver.find_element(By.XPATH, "/html/body/app-root/app-index/main/app-index/form[2]/label[12]")
+        psychology_button = self.driver.find_element(*(MainPageLocators.PHYSIOLOGY_BUTTON))
         psychology_button.click()
         time.sleep(2)
 
         assert psychology_button.get_attribute("class") == 'ng-star-inserted'
 
     def test20_procedures_button(self):
-        procedures_button = self.driver.find_element(By.XPATH, "/html/body/app-root/app-index/main/app-index/form[2]/label[13]")
+        procedures_button = self.driver.find_element(*(MainPageLocators.PROCEDURES_BUTTON))
         procedures_button.click()
         time.sleep(2)
 
         assert procedures_button.get_attribute("class") == 'ng-star-inserted'
 
     def test21_showall_button(self):
-        procedures_button = self.driver.find_element(By.XPATH, "/html/body/app-root/app-index/main/app-index/form[2]/label[13]")
+        procedures_button = self.driver.find_element(*(MainPageLocators.PROCEDURES_BUTTON))
         procedures_button.click()
         time.sleep(2)
 
-        showall_button = self.driver.find_element(By.XPATH, "/html/body/app-root/app-index/main/app-index/form[2]/button")
+        showall_button = self.driver.find_element(*(MainPageLocators.SHOWALL_BUTTON))
         showall_button.click()
         time.sleep(2)
 
